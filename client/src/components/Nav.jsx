d4a628c082e8aa24718b8724172a317fa178f2d0
Spencer Mitchell <smitch.work@gmail.com>
2017-08-24 15:20:44 -0700

    Revert "Feat/toggle logout"

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -37,12 +37,6 @@ class Navbar extends React.Component {
   }
 
   render () {
-    const isLoggedIn = this.props.isLoggedIn;
-    let logout = null;
-    if (isLoggedIn) {
-      logout = <MenuItem onClick={this.redirectTo.bind(this, '/logout')}>Logout</MenuItem>;
-    }
-    console.log('nav isLoggedIn is', isLoggedIn);
     return (
       <div>
         <AppBar
@@ -52,7 +46,7 @@ class Navbar extends React.Component {
         <Drawer docked={false} width={250} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
           <MenuItem onClick={this.redirectTo.bind(this, '/')}>Home</MenuItem>
           <MenuItem onClick={this.redirectTo.bind(this, '/profile')}>Profile</MenuItem>
-          {logout}
+          <MenuItem onClick={this.redirectTo.bind(this, '/logout')}>Logout</MenuItem>
         </Drawer>
       </div>
     )
