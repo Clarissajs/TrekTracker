7bf76c7a815610086da9ff146997c45f02ca8b02
Rebecca Chen <flyylikeaduck@gmail.com>
2017-08-25 15:47:20 -0700

    Add Router Link to /login to upload photos

--- client/src/page-components/Trail.jsx
+++ client/src/page-components/Trail.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import Paper from 'material-ui/Paper';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Posts from '../components/Posts.jsx';
 import Upload from '../components/Upload.jsx';
 
@@ -8,13 +14,16 @@ class Trail extends React.Component {
     super(props);
     this.state = {
       trailId: window.location.href.split('id=')[1],
+      trailName: null,
       posts: [],
       currentUser: null
     };
-    
+
     axios.get('/api/posts/trails/' + this.state.trailId, {params:{trailId:this.state.trailId}})
     .then((response) => {
-      this.setState({posts: response.data});
+      this.setState({
+        posts: response.data
+      });
     });
 
     axios.get('/api/currentuser')
@@ -28,7 +37,10 @@ class Trail extends React.Component {
   render() {
     return (
       <div>
-        {this.state.currentUser ? <Upload/> : <div/>}
+        <Paper>
+          <h1>Trail Name</h1>
+        </Paper>
+        {this.state.currentUser ? <Upload/> : <div><Link to='/login'>Login to upload your photos</Link></div>}
         <Posts posts={this.state.posts} />
       </div>
     );
