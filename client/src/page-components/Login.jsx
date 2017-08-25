2ae8fec6bf225a8eb3206405742e3ea0ab9fd7e7
codercc1 <swisschin@gmail.com>
2017-08-24 19:38:34 -0700

    Add in FB and Google buttons

--- client/src/page-components/Login.jsx
+++ client/src/page-components/Login.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import GoogleButton from 'react-google-button';
-import fbStyles from '../styles.css';
+//import GoogleButton from 'react-google-button';
+import '../styles.css';
 
 const Home = (props) => {
   return(
-    <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+    <div>
+    <button className="loginBtn loginBtn--facebook" onClick={() => {window.location.href = '/auth/facebook'}}>
+      Login with Facebook</button>
+    <button className="loginBtn loginBtn--google" onClick={() => {window.location.href = '/auth/google'}}>Login with Google
+    </button>
+    </div>
   )
 }
-
-// <GoogleButton className='loginBtn' onClick={() => {window.location.href = '/auth/facebook'}} />
 
 export default Home;
