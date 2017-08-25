78f913a877467ff41dd1ebafc1e05494d4e6188a
codercc1 <swisschin@gmail.com>
2017-08-24 23:29:42 -0700

    Update auth router to include fb login

--- server/auth-router.js
+++ server/auth-router.js
@@ -18,25 +18,29 @@ let isNotLoggedIn = (req, res, next) => {
   }
 };
 
-
 // Needed for Google OAuth
-// router.get('/auth/google', passport.authenticate('google', {
-//   scope: ['email', 'profile']
-// }));
-// router.get('/auth/google/callback', passport.authenticate('google', {
-//   successRedirect: '/',
-//   failureRedirect: '/login'
-// }));
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['email', 'profile']
+}));
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 // route for facebook authentication and login
-router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_friends'] }));
+
+//re-ask about any declined permissions when authenticating
+router.get('/auth/facebook',
+  passport.authenticate('facebook', { authType: 'rerequest', scope: 'user_friends' }));
 
 // handle the callback after facebook has authenticated the user
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect : '/',
-    failureRedirect : '/login'
-}));
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
 
 // Destroys current OAuth session
 router.get('/logout', (req, res) => {
