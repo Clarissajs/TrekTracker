1da6596e9b776117253740e1d6053c87e9f3bfdb
codercc1 <swisschin@gmail.com>
2017-08-24 17:55:03 -0700

    Add in facebook login functionality: need to display button properly

--- client/src/page-components/Login.jsx
+++ client/src/page-components/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import GoogleButton from 'react-google-button';
+import fbStyles from '../styles.css';
 
 const Home = (props) => {
-  return <GoogleButton className='loginBtn' onClick={() => {window.location.href = '/auth/google'}} />;
+  return(
+    <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+  )
 }
+
+// <GoogleButton className='loginBtn' onClick={() => {window.location.href = '/auth/facebook'}} />
 
 export default Home;
--- passport/google-auth-strategy.js
+++ passport/google-auth-strategy.js
@@ -1,23 +1,24 @@
 let passport = require('passport');
-let GoogleStrategy = require('passport-google-oauth2').Strategy;
+let FacebookStrategy = require('passport-facebook').Strategy;
 let users = require('../database/models.js').users;
 let config;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'test') {
-  config = require('./config.json');
+  config = require('./config.json').facebookAuth;
 }
 
 module.exports = (passport) => {
-  passport.use(new GoogleStrategy(
+  passport.use(new FacebookStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID || config.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || config.GOOGLE_CLIENT_SECRET,
+      clientID: process.env.FACEBOOK_CLIENT_ID || config.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || config.FACEBOOK_CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL || config.CALLBACK_URL,
-      passReqToCallback: true
+      passReqToCallback: true,
+      profileFields: ['id', 'emails', 'name']
     },
     (request, token, refreshToken, profile, done) => {
       process.nextTick(() => {
-        console.log(profile);
+        console.log('profile is', profile);
         users.findOne(
           {
             where: {
@@ -30,7 +31,7 @@ module.exports = (passport) => {
           } else {
             var newUser = new users();
             newUser.id = profile.id;
-            newUser.token = token;
+            //newUser.token = token;
             newUser.firstname = profile.name.givenName;
             newUser.lastname = profile.name.familyName;
             newUser.email = profile.emails[0].value;
