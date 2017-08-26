a21a5a3f3055989bb96d53e4eb1e3d43a8bc4e5e
codercc1 <swisschin@gmail.com>
2017-08-25 19:08:00 -0700

    Update posts UI

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,23 +1,71 @@
 import React from 'react'
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card'
+import { GridList, GridTile } from 'material-ui/GridList'
 import time from '../helpers/time.js'
 
+// Original code
+// const Posts = (props) => (
+//   <div>
+//     {props.posts.map((post, i) => (
+//       <div key={i}>
+//         <Card className='post'>
+//           <CardHeader
+//             title={post.poster.firstname + ' ' + post.poster.lastname}
+//             subtitle={post.poster.email}
+//           />
+//           <CardMedia overlay={<CardTitle title={post.text} subtitle={time.parse(post.createdAt, true)} />}>
+//             <img src={post['image_url']}/>
+//           </CardMedia>
+//         </Card>
+//       </div>
+//     ))}
+//   </div>
+// );
+//
+// export default Posts;
+
 const Posts = (props) => (
-  <div>
+  <div className="mdc-grid-list mdc-grid-list--twoline-caption">
+  <ul className="mdc-grid-list__tiles">
     {props.posts.map((post, i) => (
       <div key={i}>
-        <Card className='post'>
-          <CardHeader
-            title={post.poster.firstname + ' ' + post.poster.lastname}
-            subtitle={post.poster.email}
-          />
-          <CardMedia overlay={<CardTitle title={post.text} subtitle={time.parse(post.createdAt, true)} />}>
-            <img src={post['image_url']}/>
-          </CardMedia>
-        </Card>
+      <li className="mdc-grid-tile">
+        <div className="mdc-grid-tile__primary">
+          <img className="mdc-grid-tile__primary-content" src={post['image_url']} />
+        </div>
+        <span className="mdc-grid-tile__secondary">
+          <span className="mdc-grid-tile__title">{post.text}</span>
+          <span className="mdc-grid-tile__support-text">-{post.poster.firstname}</span>
+        </span>
+      </li>
       </div>
     ))}
-  </div>
+  </ul>
+</div>
 );
 
 export default Posts;
+
+// Code that works
+
+// const Posts = (props) => (
+//   <div className="mdc-grid-list mdc-grid-list--twoline-caption">
+//   <ul className="mdc-grid-list__tiles">
+//     {props.posts.map((post, i) => (
+//       <div key={i}>
+//       <li className="mdc-grid-tile">
+//         <div className="mdc-grid-tile__primary">
+//           <img className="mdc-grid-tile__primary-content" src={post['image_url']} />
+//         </div>
+//         <span className="mdc-grid-tile__secondary">
+//           <span className="mdc-grid-tile__title">{post.text}</span>
+//           <span className="mdc-grid-tile__support-text">-{post.poster.firstname}</span>
+//         </span>
+//       </li>
+//       </div>
+//     ))}
+//   </ul>
+// </div>
+// );
+//
+// export default Posts;
